ef48de811b0b74c042b4725518d9cf84252c64a0
Mutshinye <Mutshinyen@gmail.com>
2023-05-17 20:15:12 +0200

    initial changes

--- server.js
+++ server.js
@@ -1,55 +1,102 @@
 'use strict';
-
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const fccTesting  = require('./freeCodeCamp/fcctesting.js');
-
-const session= require('express-session');
-const passport=require('passport');
-
-const db=require('mongodb').MongoClient;
-const objectId=require('mongodb').ObjectID;
-
+require('dotenv').config();
+const express = require('express');
+const myDB = require('./connection');
+const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const session = require('express-session');
+const passport = require('passport');
+const routes = require('./routes.js');
+const auth = require('./auth.js');
 
 const app = express();
-app.set('view engine','pug')
-fccTesting(app); //For FCC testing purposes
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
-app.route('/')
-  .get((req, res) => {
-    res.render(process.cwd() + '/views/pug/index.pug',{title:'Hello',message:'Please login'});
-  });
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const passportSocketIo = require('passport.socketio');
+const cookieParser = require('cookie-parser');
+const MongoStore = require('connect-mongo')(session);
+const URI = process.env.MONGO_URI;
+const store = new MongoStore({ url: URI });
+
+app.set('view engine', 'pug');
+app.set('views', './views/pug');
 
 app.use(session({
-  secret:process.env.SESSION_SECRET,
-  resave:true,
-  saveUninitialized:true
-}))
-app.use(passport.initialize())
-app.use(passport.session())
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false },
+  key: 'express.sid',
+  store: store
+}));
 
-db.connect(process.env.MONGO,(err,data)=>{
-  if (err){return err}
-  else{
-   passport.serializeUser((user,done)=>{  
-     return  done(null,user._id)
-   })
+app.use(passport.initialize());
+app.use(passport.session());
 
-    passport.deserializeUser((id, done) => {
-        db.collection('user').findOne(
-            {_id: new objectId(id)},
-            (err, doc) => {
-               return  done(null, null);
-            }
-        );
+fccTesting(app); // For fCC testing purposes
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+io.use(
+  passportSocketIo.authorize({
+    cookieParser: cookieParser,
+    key: 'express.sid',
+    secret: process.env.SESSION_SECRET,
+    store: store,
+    success: onAuthorizeSuccess,
+    fail: onAuthorizeFail
+  })
+);
+
+myDB(async client => {
+  const myDataBase = await client.db('database').collection('users');
+
+  routes(app, myDataBase);
+  auth(app, myDataBase);
+
+  let currentUsers = 0;
+  io.on('connection', (socket) => {
+    ++currentUsers;
+    io.emit('user', {
+      username: socket.request.user.username,
+      currentUsers,
+      connected: true
     });
-    }
-})
+    socket.on('chat message', (message) => {
+      io.emit('chat message', { username: socket.request.user.username, message });
+    });
+    console.log('A user has connected');
+    socket.on('disconnect', () => {
+      console.log('A user has disconnected');
+      --currentUsers;
+      io.emit('user', {
+        username: socket.request.user.username,
+        currentUsers,
+        connected: false
+      });
+    });
+  });
+  
+}).catch(e => {
+  app.route('/').get((req, res) => {
+    res.render('index', { title: e, message: 'Unable to connect to database' });
+  });
+});
 
+function onAuthorizeSuccess(data, accept) {
+  console.log('successful connection to socket.io');
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Listening on port " + process.env.PORT);
+  accept(null, true);
+}
+
+function onAuthorizeFail(data, message, error, accept) {
+  if (error) throw new Error(message);
+  console.log('failed connection to socket.io:', message);
+  accept(null, false);
+}
+  
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
 });
