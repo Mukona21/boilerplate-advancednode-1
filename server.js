b8c4e8b105f42977186dcb47941bb5260a58e059
Mutshinye <Mutshinyen@gmail.com>
2023-05-18 03:03:57 +0200

    mm

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 'use strict';
+
 require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
@@ -7,21 +8,20 @@ const session = require('express-session');
 const passport = require('passport');
 const routes = require('./routes.js');
 const auth = require('./auth.js');
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const app = express();
-
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const passportSocketIo = require('passport.socketio');
 const cookieParser = require('cookie-parser');
 const MongoStore = require('connect-mongo')(session);
+
 const URI = process.env.MONGO_URI;
 
 const sessionStore = new MongoStore({
   url: URI,
   mongooseConnection: mongoose.connection,
-  // Other options...
 });
 
 app.set('view engine', 'pug');
@@ -33,7 +33,7 @@ app.use(session({
   saveUninitialized: true,
   cookie: { secure: false },
   key: 'express.sid',
-  store: sessionStore
+  store: sessionStore,
 }));
 
 app.use(passport.initialize());
@@ -51,11 +51,11 @@ io.use(
     secret: process.env.SESSION_SECRET,
     store: sessionStore,
     success: onAuthorizeSuccess,
-    fail: onAuthorizeFail
+    fail: onAuthorizeFail,
   })
 );
 
-myDB(async client => {
+myDB(async (client) => {
   const myDataBase = await client.db('database').collection('users');
 
   routes(app, myDataBase);
@@ -67,7 +67,7 @@ myDB(async client => {
     io.emit('user', {
       username: socket.request.user.username,
       currentUsers,
-      connected: true
+      connected: true,
     });
     socket.on('chat message', (message) => {
       io.emit('chat message', { username: socket.request.user.username, message });
@@ -79,29 +79,28 @@ myDB(async client => {
       io.emit('user', {
         username: socket.request.user.username,
         currentUsers,
-        connected: false
+        connected: false,
       });
     });
   });
-  
-}).catch(e => {
-  app.route('/').get((req, res) => {
-    res.render('index', { title: e, message: 'Unable to connect to database' });
+}).catch((e) => {
+  console.error('Error connecting to the database:', e);
+  app.get('/', (req, res) => {
+    res.render('index', { title: 'Error', message: 'Unable to connect to database' });
   });
 });
 
 function onAuthorizeSuccess(data, accept) {
-  console.log('successful connection to socket.io');
-
+  console.log('Successful connection to socket.io');
   accept(null, true);
 }
 
 function onAuthorizeFail(data, message, error, accept) {
+  console.error('Failed connection to socket.io:', message);
   if (error) throw new Error(message);
-  console.log('failed connection to socket.io:', message);
   accept(null, false);
 }
-  
+
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
